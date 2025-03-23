6ccde540aee5a9cec599df349e9166f2961fe0e1
Otabek Makhmudov <mahmudovotabek2208@gmail.com>
2025-03-23 07:41:32 +0500

    gsap from

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,6 +1,17 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
-
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 3,
+      ease: "power1.inOut",
+    });
+  }, []);
   return (
     <main>
       <h1>GsapFrom</h1>
