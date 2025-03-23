6586560247b65a42675ab3dadc7b573227f0c072
Otabek Makhmudov <mahmudovotabek2208@gmail.com>
2025-03-24 02:51:49 +0500

    gsap stagger

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,22 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      scale: 0.1,
+      repeat: -1,
+      stagger: {
+        amount: 1.5,
+        grid: "auto",
+        from: "random",
+        // from: "center",
+        // axis: "y",
+        // each: 0.1,
+      },
+    });
+  }, []);
 
   return (
     <main>
@@ -31,14 +48,79 @@ const GsapStagger = () => {
       </p>
 
       <div className="mt-20">
-        <div className="flex gap-5">
+        <div className="flex flex-wrap gap-5">
           <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
-          <div className="w-20 h-20 bg-indigo-800 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex flex-wrap gap-5">
+          <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
         </div>
       </div>
     </main>
